0740fbf14560c25113561388ad8b8404393e4703
Spring-Silver-Bird <adelaida_19@mail.ru>
2024-09-29 12:48:09 +0300

    step 8 add files

--- gendiff/format/choice_formatter.py
+++ gendiff/format/choice_formatter.py
@@ -1,6 +1,7 @@
 from gendiff.format.stylish import format_diff_stylish
 from gendiff.format.plain import format_diff_plain
 
+
 def format_diff(diff, formatter):
     if formatter == "stylish":
         return format_diff_stylish(diff)
@@ -8,4 +9,3 @@ def format_diff(diff, formatter):
         return format_diff_plain(diff)
     else:
         return f"Формат введен неверно: {formatter}"
-
