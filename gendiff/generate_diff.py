57fa0f1c3e18432db9f08c3f80151760cdf16567
Spring-Silver-Bird <adelaida_19@mail.ru>
2024-11-28 23:24:27 +0300

    apdate generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,10 @@
 from gendiff.parser import parse_data_from_file as make_data
 from gendiff.generate import generate
-from gendiff.format.choice_formatter import format_diff as format
+from gendiff.format.choice_formatter import format_diff
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
     old_data = make_data(file_path1)
     new_data = make_data(file_path2)
     diff = generate(old_data, new_data)
-    return format(diff, formatter)
+    return format_diff(diff, formatter)
