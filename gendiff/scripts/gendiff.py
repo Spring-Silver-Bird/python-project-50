f81f182a3eb5f3482517fb66da0e59b378a94916
Spring-Silver-Bird <adelaida_19@mail.ru>
2024-11-30 23:32:56 +0300

    fix bugs

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,10 @@
-from gendiff.diff_generator import generate_diff
+from gendiff.diff_generator import generate_diff as generate
 from gendiff.cli import get_parser_args
 
 
-def main():
+def generate_diff():
     args = get_parser_args()
-    diff = generate_diff(args.path_file1, args.path_file2, args.format)
+    diff = generate(args.path_file1, args.path_file2, args.format)
     print(diff)
 
 
